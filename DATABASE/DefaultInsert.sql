e162fb9e65e2917f055946d7cf344cda6f80c5b4
Maja <maja@lmggroup.net>
2013-11-01 14:42:26 +0100

    default insert script

---
+++ DATABASE/DefaultInsert.sql
@@ -0,0 +1,8 @@
+INSERT INTO [LMG.1POE].[dbo].[Role] ([Name]) VALUES ('Admin')
+GO
+INSERT INTO [LMG.1POE].[dbo].[Role] ([Name]) VALUES ('Worker')
+GO
+INSERT INTO [LMG.1POE].[dbo].[Language] ([Name]) VALUES ('Default')
+GO
+
+
--- DATABASE/LMG.1POE.sql
+++ DATABASE/LMG.1POE.sql
@@ -1,7 +1,79 @@
 USE [LMG.1POE]
 GO
 
-/****** Object:  Table [dbo].[Role]    Script Date: 11/01/2013 13:48:31 ******/
+/****** Object:  Table [dbo].[Theme]    Script Date: 11/01/2013 14:40:13 ******/
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+SET ANSI_PADDING ON
+GO
+
+CREATE TABLE [dbo].[Theme](
+	[ID] [int] IDENTITY(1,1) NOT NULL,
+	[Name] [varchar](50) NOT NULL,
+	[Location] [varchar](max) NOT NULL,
+ CONSTRAINT [PK_Theme] PRIMARY KEY CLUSTERED 
+(
+	[ID] ASC
+)WITH (PAD_INDEX  = OFF, STATISTICS_NORECOMPUTE  = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS  = ON, ALLOW_PAGE_LOCKS  = ON) ON [PRIMARY]
+) ON [PRIMARY]
+
+GO
+
+SET ANSI_PADDING OFF
+GO
+
+
+/****** Object:  Table [dbo].[Language]    Script Date: 11/01/2013 14:40:35 ******/
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+SET ANSI_PADDING ON
+GO
+
+CREATE TABLE [dbo].[Language](
+	[ID] [int] IDENTITY(1,1) NOT NULL,
+	[Name] [varchar](50) NOT NULL,
+ CONSTRAINT [PK_Language] PRIMARY KEY CLUSTERED 
+(
+	[ID] ASC
+)WITH (PAD_INDEX  = OFF, STATISTICS_NORECOMPUTE  = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS  = ON, ALLOW_PAGE_LOCKS  = ON) ON [PRIMARY]
+) ON [PRIMARY]
+
+GO
+
+SET ANSI_PADDING OFF
+GO
+
+/****** Object:  Table [dbo].[Country]    Script Date: 11/01/2013 14:40:52 ******/
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+CREATE TABLE [dbo].[Country](
+	[ID] [int] IDENTITY(1,1) NOT NULL,
+	[Name] [nvarchar](50) NOT NULL,
+	[LanguageID] [int] NOT NULL
+) ON [PRIMARY]
+
+GO
+
+ALTER TABLE [dbo].[Country]  WITH CHECK ADD  CONSTRAINT [FK_Country_Language] FOREIGN KEY([LanguageID])
+REFERENCES [dbo].[Language] ([ID])
+GO
+
+ALTER TABLE [dbo].[Country] CHECK CONSTRAINT [FK_Country_Language]
+GO
+
+/****** Object:  Table [dbo].[Role]    Script Date: 11/01/2013 14:41:13 ******/
 SET ANSI_NULLS ON
 GO
 
@@ -31,7 +103,9 @@ GO
 
 ALTER TABLE [dbo].[Role] CHECK CONSTRAINT [FK_Role_Role]
 GO
-/****** Object:  Table [dbo].[User]    Script Date: 11/01/2013 13:50:27 ******/
+
+
+/****** Object:  Table [dbo].[User]    Script Date: 11/01/2013 14:41:36 ******/
 SET ANSI_NULLS ON
 GO
 
@@ -55,6 +129,8 @@ CREATE TABLE [dbo].[User](
 	[CountryID] [int] NULL,
 	[Telephone] [varchar](20) NULL,
 	[MobilePhone] [varchar](20) NULL,
+	[DefaultLanguageID] [int] NULL,
+	[DefaultThemeID] [int] NULL,
  CONSTRAINT [PK_User] PRIMARY KEY CLUSTERED 
 (
 	[ID] ASC
@@ -66,8 +142,21 @@ GO
 SET ANSI_PADDING OFF
 GO
 
+ALTER TABLE [dbo].[User]  WITH CHECK ADD  CONSTRAINT [FK_User_Language] FOREIGN KEY([DefaultLanguageID])
+REFERENCES [dbo].[Language] ([ID])
+GO
 
-/****** Object:  Table [dbo].[UserInRole]    Script Date: 11/01/2013 13:50:49 ******/
+ALTER TABLE [dbo].[User] CHECK CONSTRAINT [FK_User_Language]
+GO
+
+ALTER TABLE [dbo].[User]  WITH CHECK ADD  CONSTRAINT [FK_User_Theme] FOREIGN KEY([DefaultThemeID])
+REFERENCES [dbo].[Theme] ([ID])
+GO
+
+ALTER TABLE [dbo].[User] CHECK CONSTRAINT [FK_User_Theme]
+GO
+
+/****** Object:  Table [dbo].[UserInRole]    Script Date: 11/01/2013 14:41:54 ******/
 SET ANSI_NULLS ON
 GO
 
@@ -94,4 +183,3 @@ GO
 
 ALTER TABLE [dbo].[UserInRole] CHECK CONSTRAINT [FK_UserInRole_User]
 GO
-
